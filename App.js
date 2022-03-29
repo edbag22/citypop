f8fff53e7dfca82c71afd45a6d68cc9a18f4b223
Edvin Baggman <edvinbaggman@hotmail.se>
2022-03-29 12:46:04 +0200

    initial layout and functionality

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, Button, TouchableHighlight} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from "./app/screens/homeScreen";
+import SearchScreen from "./app/screens/searchScreen";
+import ResultScreen from "./app/screens/resultScreen";
+import CountriesResultScreen from "./app/screens/countriesResultScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return(
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Search" component={SearchScreen} />
+        <Stack.Screen name="Result" component={ResultScreen} />
+        <Stack.Screen name="CountriesResult" component={CountriesResultScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
