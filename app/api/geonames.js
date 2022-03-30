201d34867ae6b79cd3cdab03bbbc3547703242c7
Edvin Baggman <edvinbaggman@hotmail.se>
2022-03-30 13:42:58 +0200

    improved api call parameters

--- app/api/geonames.js
+++ app/api/geonames.js
@@ -1,6 +1,6 @@
 const GeoNames={
-  searchCity(city){ // name of city
-    return fetch(`http://api.geonames.org/search?maxRows=1&type=json&featureClass=p&username=weknowit&q=`+ city)
+  apiCall(params){ //Making a call to the GeoNames API with the selected parameters
+    return fetch("http://api.geonames.org/search?type=json&username=weknowit&" + params)
           .then(response => {
             if(response.status !== 200) {
               throw response.status
@@ -8,28 +8,16 @@ const GeoNames={
             return response
           })
           .then(response => response.json());
-    },
-    searchCountry(country){ // name of country
-      return fetch(`http://api.geonames.org/search?maxRows=1&type=json&featureClass=a&username=weknowit&q=`+ country)
-            .then(response => {
-              if(response.status !== 200) {
-                throw response.status
-              }
-              return response
-            })
-            .then(response => response.json());
-          },
-    searchCities(countryCode){ // country code in ISO3166
-      console.log(countryCode);
-      return fetch(`http://api.geonames.org/search?maxRows=3&type=json&featureClass=p&username=weknowit&q=&country=`+ countryCode)
-            .then(response => {
-              if(response.status !== 200) {
-                throw response.status
-              }
-              return response
-            })
-            .then(response => response.json());
-    }
+  },
+  searchCity(city){ // Searching for a specific city, search with the name of the city
+    return GeoNames.apiCall(`maxRows=1&featureClass=p&name=${encodeURI(city)}`)
+  },
+  searchCountry(country){ // Searching for a country, search with the name of the country
+    return GeoNames.apiCall(`maxRows=1&featureClass=a&featureCode=PCLI&name=${encodeURI(country)}`)
+  },
+  searchCities(countryCode){ // Searching for the 3 biggest cities in the country, search with country code in ISO3166
+    return GeoNames.apiCall(`maxRows=3&orderby=population&featureClass=p&q=&country=`+ countryCode)
+  }
 }
 
 export default GeoNames;
